d15d60d1025a9c83471653eb40ef789d914cbf07
Constantin Petra <constantin.petra@yahoo.com>
2021-12-16 20:17:44 +0200

    Added code.

---
+++ SizedQueue.h
@@ -0,0 +1,94 @@
+/******************************************************************************
+ * @Date    : 2021-12-15 05:08:55
+ * @Author  : Constantin Petra (constantin.petra@gmail.com)
+ * @Link    : http://github.com/cpetra
+ * @Version : 0.1
+ *
+ *****************************************************************************/
+
+#ifndef __SIZED_QUEUE_H_INCLUDED
+#define __SIZED_QUEUE_H_INCLUDED
+
+#if defined(ESP8266) || defined(ESP32)
+#define HAVE_SEMAPHORE
+#include <freertos/semphr.h>
+#endif
+
+template <typename T> 
+class SizedQueue {
+    T *q;
+    size_t size;
+    size_t last;
+    size_t first;
+#ifdef HAVE_SEMAPHORE
+    SemaphoreHandle_t sem;
+#endif
+    inline void lock() {
+        #if defined(HAVE_SEMAPHORE)
+        xSemaphoreTake(sem, 0);
+        #endif
+    }
+
+    inline void unlock() {
+        #if defined(HAVE_SEMAPHORE)
+        xSemaphoreGive(sem);
+        #endif
+    }
+
+public:
+    SizedQueue(int num) {
+        q = new T[num];
+        last = first = 0;
+        size = num;
+        #if defined(HAVE_SEMAPHORE)
+        sem = xSemaphoreCreateMutex();
+        #endif
+    }
+
+    ~SizedQueue() {
+        delete[] q;
+        #if defined(HAVE_SEMAPHORE)
+        vSemaphoreDelete(sem);
+        #endif
+    }
+
+    void push(const T&a) {
+        lock();
+        q[last] = a;
+        if(++last >= size) {
+            last = 0;
+        }
+
+        if (first == last) {
+            if (++first >= size) {
+                first = 0;
+            }
+        }
+        unlock();
+    }
+
+    bool pop(T& a) {
+        lock();
+        if (last == first) {
+            unlock();
+            return false;
+        }
+
+        a = q[first];
+        if (++first >= size) {
+            first = 0;
+        }
+        unlock();
+        return true;
+    }
+
+    bool peek() {
+        bool ret;
+        lock();
+        ret = !(last == first);
+        unlock();
+        return ret;
+    }
+};
+#endif /*__SIZED_QUEUE_H_INCLUDED*/
+
---
+++ examples/async_read_show/async_read_show.ino
@@ -0,0 +1,62 @@
+/******************************************************************************
+ * @Date    : 2021-12-16 05:52:43
+ * @Author  : Constantin Petra (constantin.petra@gmail.com)
+ * @Link    : http://github.com/cpetra
+ * @Version : 0.1
+ * 
+******************************************************************************/
+
+#include <SizedQueue.h>
+#include <thread>
+
+/* Simple example for using SizedQueue:
+ *  - in the main loop, push an item at each 100ms.
+ *  - in the "show" thread, pop everything available each second, and print the result.
+ */
+ 
+static std::thread thread_show;
+
+/* demo struct, can be anything
+ *  
+ */
+typedef struct {
+    int v1;
+    int v2;
+} _svals;
+
+/* The queue, capable of holding 2x the expected fill rate
+ *  
+ */
+SizedQueue<_svals> m_queue(20);
+
+static void show()
+{
+  _svals v;
+
+  while (1) {
+    while (m_queue.pop(v)) {
+      Serial.print(" " + String(v.v1) + "," + String(v.v2));
+    }
+    Serial.println("");
+    delay(1000);
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  thread_show = std::thread(show);
+  randomSeed(analogRead(0));
+}
+
+
+int i = 0;
+
+void loop()
+{
+  _svals v;
+  v.v1 = i++;
+  v.v2 = random(100);
+  m_queue.push(v);
+  delay(100);
+}
